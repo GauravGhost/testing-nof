eaa6da75bfaf761855d37b091399180424eae362
Gyanendra Kumar <51630728+GauravGhost@users.noreply.github.com>
2020-06-08 23:51:02 +0530

    Added link of fasttrack

---
+++ assets/js/gplinks.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+    var app_url = 'https://gplinks.in/';
+    var app_api_token = '701c88bead906e510eeda9d8b91ec76782f2df0b';
+    var app_advert = 2;
+    var app_domains = ["onedrive.live.com"];
+</script>
+<script src='//gplinks.in/js/full-page-script.js'></script>
